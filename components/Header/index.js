2e62aef485c6bab00266d9a439d33c3f65e5f5bf
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-04 08:15:12 -0700

    Completed Header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,31 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+  // Create elements
+  const headingContainer = document.querySelector(".header-container"),
+    header = document.createElement("div"),
+    date = document.createElement("span"),
+    headingTitle = document.createElement("h1"),
+    temperature = document.createElement("span");
+
+  // Add Classes
+  header.classList.add("header");
+  date.classList.add("date");
+  temperature.classList.add("temp");
+
+  // Add Content
+  date.textContent = "March 28, 2019";
+  headingTitle.textContent = "Lambda Times";
+  temperature.textContent = "98°";
+
+  // Append Items
+  headingContainer.appendChild(header);
+  header.appendChild(date);
+  header.appendChild(headingTitle);
+  header.appendChild(temperature);
+
+  return header;
+}
+
+Header();
